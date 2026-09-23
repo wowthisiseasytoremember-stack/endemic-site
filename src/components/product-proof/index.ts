04e508e03c659f5d5c2fc57535f375edbd8336c2
Justin Ryan <wowthisiseasytoremember@gmail.com>
2026-09-22 21:27:59 -0700

    visual: add product-proof presentation kit (index.ts)

---
+++ src/components/product-proof/index.ts
@@ -0,0 +1,19 @@
+export {
+  CorrectionCard,
+  DestinationState,
+  EvidenceReceipt,
+  MysteryCard,
+  ProofGrid,
+  StoryLead,
+  SubjectIdentity,
+  ThreadCard,
+} from "./StoryPrimitives";
+
+export type { DestinationState as DestinationStateType, EvidenceState } from "./StoryPrimitives";
+
+export {
+  EvidenceExpandCue,
+  StoryReveal,
+  ThreadSequence,
+  ThreadStep,
+} from "./StoryMotion";
