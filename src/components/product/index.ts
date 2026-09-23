c711d5a98aba39bb95b3b0a56cf83f683c9a49fb
Justin Ryan <wowthisiseasytoremember@gmail.com>
2026-09-22 21:35:16 -0700

    design: consolidate index.ts into rabbit-hole kit

---
+++ src/components/product/index.ts
@@ -0,0 +1,29 @@
+export {
+  CorrectionCard,
+  DocumentCard,
+  EvidenceReceipt,
+  RelationshipFact,
+  SubjectIdentity,
+  ThreadLink,
+  UnknownCard,
+} from "./RabbitHolePrimitives";
+
+export type {
+  RabbitHoleAccent,
+  ThreadState,
+} from "./RabbitHolePrimitives";
+
+export {
+  RabbitHoleReveal,
+} from "./RabbitHoleMotion";
+
+export type {
+  RabbitHoleMotionKind,
+} from "./RabbitHoleMotion";
+
+export {
+  RabbitHoleLoading,
+  ResearchingState,
+  SourceShelf,
+  UnavailableState,
+} from "./RabbitHoleStates";
